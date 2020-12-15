4981f2f27028b22a41b27eb0379c8ff91dbcaab4
Alexanderfilip00 <61449121+Alexanderfilip00@users.noreply.github.com>
2020-12-15 13:01:09 +0100

    pred premiou

---
+++ postfix.py
@@ -0,0 +1,136 @@
+def eval_expr(s,d={}):
+
+	cisla= []
+	
+	for k in ( s.split() ):
+		if k=="+":
+			a = cisla.pop(0)
+			if a in d:
+				a=d[a]
+			b = cisla.pop(0)
+			if b in d:
+				b=d[b]
+			cisla.append(int(a) + int(b))
+			
+		elif k=="-":
+			a = cisla.pop(0)
+			if a in d:
+				a=d[a]
+			b = cisla.pop(0)
+			if b in d:
+				b=d[b]
+				
+			cisla.append(int(a) - int(b))
+			
+		elif k=="*":
+			a = cisla.pop(0)
+			if a in d:
+				a=d[a]
+			b = cisla.pop(0)
+			if b in d:
+				b=d[b]
+			cisla.append(int(a) * int(b))
+			
+		elif k=="/":
+			a = cisla.pop(0)
+			if a in d:
+				a=d[a]
+			b = cisla.pop(0)
+			if b in d:
+				b=d[b]
+			cisla.append(int(a) / int(b))
+			
+		else:
+			cisla.append(k)
+	
+	return(cisla[0])
+	
+	
+	
+	
+	
+def to_infix(s):
+	s = s.replace(' ','')
+	to_ret=''
+	znaky=[]
+	
+	for i in s:
+		if i == '+':
+			znaky.insert(0,"( ")
+			znaky.insert(-1," + ")
+			znaky.append(" ) ")
+					
+		elif i == '-':
+			znaky.insert(0,"( ")
+			znaky.insert(-1," - ")
+			znaky.append(" ) ")
+			
+		elif i == '*':
+			znaky.insert(0,"( ")
+			znaky.insert(-1," * ")
+			znaky.append(" ) ")
+			
+		elif i == '/':
+			znaky.insert(0,"( ")
+			znaky.insert(-1," / ")
+			znaky.append(" ) ")
+			
+		else:
+
+			znaky.append(i)
+	
+	for j in znaky:
+		to_ret = to_ret + j
+		
+	return to_ret
+
+
+def to_postfix()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
